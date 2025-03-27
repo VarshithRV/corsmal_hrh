ea24f9f6403a08fa8f737b5b83db15ae22ffad23
Varshith R V <vrv278@gmail.com>
2025-03-28 01:45:17 +0800

    pretty much the last commit ig

--- corsmal_setup_bringup/scripts/get_transformation.py
+++ corsmal_setup_bringup/scripts/get_transformation.py
@@ -1,47 +1,47 @@
 #!/usr/bin/env python
-
 import rospy
 import tf
 from tf import transformations as tfs
 import os
 import json
+import argparse
 
 def get_transformation(frame_a, frame_b):
     rospy.init_node('tf_listener_node', anonymous=True)
-
     listener = tf.TransformListener()
-    
-    rate = rospy.Rate(10.0)  # 10 Hz
-
+    rate = rospy.Rate(10.0) # 10 Hz
     while not rospy.is_shutdown():
         try:
             # Wait for tf transform from frame_a to frame_b
             (trans, rot) = listener.lookupTransform(frame_a, frame_b, rospy.Time(0))
-
             # Output translation and rotation
             rospy.loginfo(f"Translation: {trans}")
             rospy.loginfo(f"Rotation (quaternion): {rot}")
-
             # Convert quaternion to Euler angles for better understanding
             euler = tfs.euler_from_quaternion(rot)
             rospy.loginfo(f"Rotation (euler): {euler}")
             return trans, rot
             break
-
-        except Exception as e:
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
             continue
 
+def main():
+    parser = argparse.ArgumentParser(description='Retrieve and save transformation between two frames.')
+    parser.add_argument('source_frame', type=str, help='The source frame.')
+    parser.add_argument('target_frame', type=str, help='The target frame.')
+    parser.add_argument('output_filename', type=str, help='The output file to save the transformation.')
+    
+    args = parser.parse_args()
 
-if __name__ == '__main__':
     try:
-        frame_b = 'rightcamera_base'
-        frame_a = 'handeye_target'
-        trans,rot = get_transformation(frame_a, frame_b)
-        # Save the transformation to a file in the ../config/ directory
-        config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../config/transformation.json')
-        transformation = {"translation":trans,"rotation":rot}
-        transformation_json = json.dumps(transformation)
+        trans, rot = get_transformation(args.source_frame, args.target_frame)
+        # Save the transformation to the specified output file
+        config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../config/', "args.output_filename"+".json")
+        transformation = {"translation": trans, "rotation": rot}
         with open(config_path, 'w') as file:
-            file.write(transformation_json)
+            json.dump(transformation, file, indent=4)
     except rospy.ROSInterruptException:
         pass
+
+if __name__ == '__main__':
+    main()
