4f63d35ecb8e6de819e8f38e41acce5260ced0c7
Varshith R V <vrv278@gmail.com>
2025-04-23 03:41:02 +0800

    april tag detection in progress

--- aruco_proxy_pose_estimation/scripts/realsense_camera_sample_code.py
+++ aruco_proxy_pose_estimation/scripts/realsense_camera_sample_code.py
@@ -1,72 +1,53 @@
+# sample code for realsense type camera
+# cv_bridge documentation : https://wiki.ros.org/cv_bridge/Tutorials/ConvertingBetweenROSImagesAndOpenCVImagesPython
 import rospy
 from sensor_msgs.msg import CameraInfo, Image
 import cv_bridge, cv2
 import numpy as np
 from image_geometry import PinholeCameraModel
 
-# Camera identifiers
-LEFT_CAMERA = "rs_415_left"
-RIGHT_CAMERA = "rs_415_right"
-
 class Deprojection:
     def __init__(self) -> None:
-        # Initialize camera-related variables
-        self.left_depth_image = None
-        self.left_camera_info = None
-        self.left_color_image = None
-        self.right_depth_image = None
-        self.right_camera_info = None
-        self.right_color_image = None
+        self.depth_image = None
+        self.camera_info = None
+        self.color_image = None
         self.cv_bridge = cv_bridge.CvBridge()
-        self.left_camera_model = PinholeCameraModel()
-        self.right_camera_model = PinholeCameraModel()
+        self.camera_model = PinholeCameraModel()
         
         # Camera topics
-        left_camera_color_topic = f"/left/{LEFT_CAMERA}/color/image_raw"
-        left_camera_info_topic = f"/left/{LEFT_CAMERA}/aligned_depth_to_color/camera_info"
-        left_camera_depth_topic = f"/left/{LEFT_CAMERA}/aligned_depth_to_color/image_raw"
-        right_camera_color_topic = f"/right/{RIGHT_CAMERA}/color/image_raw"
-        right_camera_info_topic = f"/right/{RIGHT_CAMERA}/aligned_depth_to_color/camera_info"
-        right_camera_depth_topic = f"/right/{RIGHT_CAMERA}/aligned_depth_to_color/image_raw"
+        camera_color_topic = f"/camera/color/image_raw"
+        camera_info_topic = f"/camera/aligned_depth_to_color/camera_info"
+        camera_depth_topic = f"/camera/aligned_depth_to_color/image_raw"
         
         # Subscribers
-        self.left_depth_image_sub = rospy.Subscriber(left_camera_depth_topic, Image, self.left_depth_image_callback)
-        self.left_camera_info_sub = rospy.Subscriber(left_camera_info_topic, CameraInfo, self.left_camera_info_callback)
-        self.left_color_image_sub = rospy.Subscriber(left_camera_color_topic, Image, self.left_color_image_callback)
-        self.right_depth_image_sub = rospy.Subscriber(right_camera_depth_topic, Image, self.right_depth_image_callback)
-        self.right_camera_info_sub = rospy.Subscriber(right_camera_info_topic, CameraInfo, self.right_camera_info_callback)
-        self.right_color_image_sub = rospy.Subscriber(right_camera_color_topic, Image, self.right_color_image_callback)
+        self.depth_image_sub = rospy.Subscriber(camera_depth_topic, Image, self.depth_image_callback)
+        self.camera_info_sub = rospy.Subscriber(camera_info_topic, CameraInfo, self.camera_info_callback)
+        self.color_image_sub = rospy.Subscriber(camera_color_topic, Image, self.color_image_callback)
 
-
-
+        # Timers
+        self.display_color_image_timer = rospy.Timer(rospy.Duration(0.05),callback=self.display_color_image)
         
     def __del__(self):
-        del self.model
+        del self.camera_model
 
-    def left_color_image_callback(self, msg: Image):
-        self.left_color_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+    def display_color_image(self, event):
+        if self.color_image is not None :
+            cv2.imshow("annotated_image",self.color_image)
+            cv2.waitKey(1)
 
-    def left_depth_image_callback(self, msg: Image):
-        self.left_depth_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+    def color_image_callback(self, msg: Image):
+        self.color_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding="mono16") # change to bgr8 or rgb8 for color
 
-    def left_camera_info_callback(self, msg: CameraInfo):
-        self.left_camera_info = msg
-        self.left_camera_model.fromCameraInfo(msg)
+    def depth_image_callback(self, msg: Image):
+        self.depth_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
 
-    def right_color_image_callback(self, msg: Image):
-        self.right_color_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
-
-    def right_depth_image_callback(self, msg: Image):
-        self.right_depth_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
-
-    def right_camera_info_callback(self, msg: CameraInfo):
-        self.right_camera_info = msg
-        self.right_camera_model.fromCameraInfo(msg)
+    def camera_info_callback(self, msg: CameraInfo):
+        self.camera_info = msg
+        self.camera_model.fromCameraInfo(msg)
 
 if __name__ == "__main__":
-    rospy.init_node("deprojection_node")
-    rospy.sleep(0.5)
+    rospy.init_node("realsense_sample_node")
+    rospy.sleep(0.15) # some time to initialize stuff
     deproject = Deprojection()
-    rospy.sleep(0.5)
-    rospy.loginfo("Deproject server ready...")
+    rospy.loginfo("Node running")
     rospy.spin()
