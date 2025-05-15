9d4c5a84b8305fb0c5ed514b0a191d5757bda86a
Varshith R V <vrv278@gmail.com>
2025-05-16 03:35:01 +0800

    client updated to use hand pose for two types of wait

--- grasp_control_actions/scripts/client.py
+++ grasp_control_actions/scripts/client.py
@@ -13,6 +13,7 @@ import threading
 import copy
 from std_msgs.msg import Float32
 from gripper_driver.srv import SetGripper, SetGripperRequest
+import numpy as np
 
 '''
 rest state
@@ -31,6 +32,7 @@ ALPHA = 0.5 # more means trust filtered data more
 Z_DISPLACEMENT = 0.10 #m, should reduce to make it faster
 MAX_VELOCITY_THRESHOLD = 0.0 #ms-1, should reduce to make it faster
 HANDOVER_VELOCITY_THRESHOLD = 0.5 #ms-1 should increase to make it faster
+HAND_PROXIMITY_THRESHOLD = 0.05 # m, should be higher to start sooner
 
 class MpClass:
     def __init__(self):
@@ -47,10 +49,16 @@ class MpClass:
         self.fgp_list = []
         self.fgp_linear_velocity = Twist()
         self.fgp_linear_velocity_filtered = Twist()
+        self.hand = None
+        self.hand_inital = None
+        self.hand_list = []
+        self.hand_linear_velocity = Twist()
+        self.hand_linear_velocity_filtered = Twist()
         self._velocity_filter_coeff = ALPHA
         self.array_access_lock = threading.Lock()
         self._max_vel_z = 0.0
         self.start_handover = 0.0 # this is just for debugging
+        self.hand_pose = None
         
         # connect to action servers
         rospy.loginfo("%s Started client, connecting to action servers", rospy.get_name())
@@ -79,10 +87,14 @@ class MpClass:
         self.yoink_feedback_sub = rospy.Subscriber("/yoink/linear_error",Float32,callback=self.yoink_feedback_cb)
         self.yoink_feedback_sub = rospy.Subscriber("/place_br/linear_error",Float32,callback=self.place_br_feedback_cb)
         self.fgp_sub = rospy.Subscriber("/filtered_grasp_pose",PoseStamped,callback=self.fgp_cb)
-        self.fgp_linear_velocity_publisher = rospy.Publisher("/filtered_grasp_pose_linear_velocity",Twist,queue_size=1)
+        self.hand_pose_sub = rospy.Subscriber("/hand_filtered_pose",PoseStamped,callback=self.hand_pose_cb)
         self.start_handover_publisher = rospy.Publisher("/start_handover",Float32,queue_size=1)
+        self.fgp_linear_velocity_publisher = rospy.Publisher("/filtered_grasp_pose_linear_velocity",Twist,queue_size=1)
         self.calculate_fgp_linear_velocity_timer = rospy.Timer(rospy.Duration(1/30),self.calculate_fgp_linear_velocity_cb)
         self.filter_fgp_vel_timer = rospy.Timer(rospy.Duration(1/30),self.filter_fgp_vel_cb)
+        self.hand_linear_velocity_publisher = rospy.Publisher("/hand_pose_linear_velocity",Twist,queue_size=1)
+        self.calculate_hand_linear_velocity_timer = rospy.Timer(rospy.Duration(1/30),self.calculate_hand_linear_velocity_cb)
+        self.filter_hand_vel_timer = rospy.Timer(rospy.Duration(1/30),self.filter_hand_vel_cb)
 
         rospy.loginfo("Waiting for one fgp message")
         rospy.wait_for_message("/filtered_grasp_pose",PoseStamped)
@@ -124,11 +136,44 @@ class MpClass:
         self.fgp_linear_velocity_filtered.linear.y  = (1-self._velocity_filter_coeff)*self.fgp_linear_velocity.linear.y + self._velocity_filter_coeff*self.fgp_linear_velocity_filtered.linear.y
         self.fgp_linear_velocity_filtered.linear.z  = (1-self._velocity_filter_coeff)*self.fgp_linear_velocity.linear.z + self._velocity_filter_coeff*self.fgp_linear_velocity_filtered.linear.z
     
+    def calculate_hand_linear_velocity_cb(self,event):
+        with self.array_access_lock:
+            if len(self.hand_list)-1 > 0:
+                if len(self.hand_list) < VELOCITY_AVG_WINDOW_SIZE:
+                    delta_t = self.hand_list[-1].header.stamp.to_sec() - self.hand_list[0].header.stamp.to_sec()
+                    delta_x = self.hand_list[-1].pose.position.x - self.hand_list[0].pose.position.x
+                    delta_y = self.hand_list[-1].pose.position.y - self.hand_list[0].pose.position.y
+                    delta_z = self.hand_list[-1].pose.position.z - self.hand_list[0].pose.position.z
+
+                    self.hand_linear_velocity.linear.x = delta_x/delta_t
+                    self.hand_linear_velocity.linear.y = delta_y/delta_t
+                    self.hand_linear_velocity.linear.z = delta_z/delta_t
+                else :
+                    delta_t = self.hand_list[-1].header.stamp.to_sec() - self.hand_list[-VELOCITY_AVG_WINDOW_SIZE].header.stamp.to_sec()
+                    delta_x = self.hand_list[-1].pose.position.x - self.hand_list[-VELOCITY_AVG_WINDOW_SIZE].pose.position.x
+                    delta_y = self.hand_list[-1].pose.position.y - self.hand_list[-VELOCITY_AVG_WINDOW_SIZE].pose.position.y
+                    delta_z = self.hand_list[-1].pose.position.z - self.hand_list[-VELOCITY_AVG_WINDOW_SIZE].pose.position.z
+
+                    self.hand_linear_velocity.linear.x = delta_x/delta_t
+                    self.hand_linear_velocity.linear.y = delta_y/delta_t
+                    self.hand_linear_velocity.linear.z = delta_z/delta_t
+        if self.hand_linear_velocity.linear.z > self._max_vel_z:
+            self._max_vel_z = self.hand_linear_velocity.linear.z
+
+    def filter_hand_vel_cb(self,event):
+        self.hand_linear_velocity_filtered.linear.x  = (1-self._velocity_filter_coeff)*self.hand_linear_velocity.linear.x + self._velocity_filter_coeff*self.hand_linear_velocity_filtered.linear.x
+        self.hand_linear_velocity_filtered.linear.y  = (1-self._velocity_filter_coeff)*self.hand_linear_velocity.linear.y + self._velocity_filter_coeff*self.hand_linear_velocity_filtered.linear.y
+        self.hand_linear_velocity_filtered.linear.z  = (1-self._velocity_filter_coeff)*self.hand_linear_velocity.linear.z + self._velocity_filter_coeff*self.hand_linear_velocity_filtered.linear.z
+    
+
     def yoink_feedback_cb(self,msg):
         self.yoink_feedback = msg
 
     def place_br_feedback_cb(self,msg):
         self.place_br_feedback = msg
+
+    def hand_pose_cb(self,msg):
+        self.hand_pose = msg
 
     def fgp_cb(self,msg):
         self.fgp = msg
@@ -136,6 +181,33 @@ class MpClass:
             self.fgp_list.append(self.fgp)
         self.fgp_linear_velocity_publisher.publish(self.fgp_linear_velocity_filtered)
         self.start_handover_publisher.publish(Float32(data=self.start_handover))
+
+    def wait_for_handover_hand_proximity_based(self):
+        rate = 30
+        while not rospy.is_shutdown():
+            hand_position = np.array([self.hand_pose.pose.position.x,self.hand_pose.pose.position.y,self.hand_pose.pose.position.z],dtype=float)
+            object_position = np.array([self.fgp.pose.position.x,self.fgp.pose.position.y,self.fgp.pose.position.z],dtype=float)
+            delta_d = np.linalg.norm(hand_position-object_position)
+            if delta_d < HAND_PROXIMITY_THRESHOLD:
+                break
+            rate.sleep()
+
+    def wait_for_handover_hand_vel_based(self):
+        rate = rospy.Rate(50)
+        start = rospy.get_time()
+
+        while not rospy.is_shutdown():
+            if (self.hand.pose.position.z - self.hand_inital.pose.position.z > Z_DISPLACEMENT and 
+                self.hand_linear_velocity_filtered.linear.z < HANDOVER_VELOCITY_THRESHOLD and 
+                self._max_vel_z > MAX_VELOCITY_THRESHOLD):
+                self.start_handover = 0.5
+                break
+            else :
+                self.start_handover = 0.0
+
+            rate.sleep()
+
+        rospy.loginfo(f"Done waiting for handover, waited {rospy.get_time()-start}")
 
     def wait_for_handover(self):
         rate = rospy.Rate(50)
